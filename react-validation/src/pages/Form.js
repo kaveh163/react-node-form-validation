e56f99898f810f50fdc782c2319d77ac432aafbf
Kaveh <kavehfrounchi@gmail.com>
2022-05-14 16:21:35 +0430

    Fixed the view of the app in mobile screens. Added container class to the Form componentin jsx

--- react-validation/src/pages/Form.js
+++ react-validation/src/pages/Form.js
@@ -82,142 +82,143 @@ function Form() {
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        if(data.errors) {
+        if (data.errors) {
           setData(data);
         } else {
           window.location.href = data.url;
         }
-        
       });
   };
   // console.log("return");
   return (
     <>
-      <section className="row mt-2">
-        <section className="col-md-6 offset-md-3" id="formWrapper">
-          <form
-            action="https://www.google.com/"
-            className="row g-3 was-validated"
-          >
-            <div className="col-12">
-              <label htmlFor="username" className="form-label">
-                Username
-              </label>
-              <div className="input-group has-validation">
-                <span className="input-group-text">@</span>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="username"
-                  name="username"
-                  ref={userElement}
-                  required
-                />
-                <div id="UsernameFeedback" className="invalid-feedback">
-                  Please provide username.
+      <section className="container mt-3">
+        <section className="row m-0 p-0">
+          <section className="col-md-6 offset-md-3" id="formWrapper">
+            <form
+              action="https://www.google.com/"
+              className="row g-3 was-validated"
+            >
+              <div className="col-12">
+                <label htmlFor="username" className="form-label">
+                  Username
+                </label>
+                <div className="input-group has-validation">
+                  <span className="input-group-text">@</span>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="username"
+                    name="username"
+                    ref={userElement}
+                    required
+                  />
+                  <div id="UsernameFeedback" className="invalid-feedback">
+                    Please provide username.
+                  </div>
+                  <div className="valid-feedback"></div>
                 </div>
-                <div className="valid-feedback"></div>
               </div>
-            </div>
 
-            <div className="col-12">
-              <label htmlFor="password" className="form-label">
-                Password
-              </label>
-              <div className="input-group has-validation">
+              <div className="col-12">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <div className="input-group has-validation">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    name="password"
+                    ref={passElement}
+                    required
+                  />
+                  <span className="input-group-text" ref={spanElement}>
+                    <FontAwesomeIcon
+                      icon={isEye ? "eye" : "eye-slash"}
+                      onClick={handleEyeIcon}
+                    />
+                  </span>
+                  <div id="PasswordFeedback" className="invalid-feedback">
+                    {data
+                      ? data.errors.map((item, index) => {
+                          if (item.param === "password") {
+                            passElement.current.value = "";
+                            // console.log(item.msg);
+                            return `${item.msg}. `;
+                          } else {
+                            return "";
+                          }
+                        })
+                      : "Password must be minimum 8 characters. Password must include uppercase,lowercase letters, digits and symbols"}
+                  </div>
+                  <div className="valid-feedback"></div>
+                </div>
+              </div>
+              <div className="col-12">
+                <label htmlFor="passConfirm" className="form-label">
+                  Confirm password
+                </label>
                 <input
                   type="password"
                   className="form-control"
-                  id="password"
-                  name="password"
-                  ref={passElement}
+                  id="passConfirm"
+                  name="passConfirm"
+                  ref={confirmElement}
                   required
                 />
-                <span className="input-group-text" ref={spanElement}>
-                  <FontAwesomeIcon
-                    icon={isEye ? "eye" : "eye-slash"}
-                    onClick={handleEyeIcon}
-                  />
-                </span>
-                <div id="PasswordFeedback" className="invalid-feedback">
+                <div className="invalid-feedback" id="ConfirmPasswordFeedback">
                   {data
                     ? data.errors.map((item, index) => {
-                        if (item.param === "password") {
-                          passElement.current.value = "";
-                          // console.log(item.msg);
+                        if (item.param === "confirmPass") {
+                          confirmElement.current.value = "";
                           return `${item.msg}. `;
                         } else {
                           return "";
                         }
                       })
-                    : "Password must be minimum 8 characters. Password must include uppercase,lowercase letters, digits and symbols"}
+                    : "confirm password must match password"}
                 </div>
                 <div className="valid-feedback"></div>
               </div>
-            </div>
-            <div className="col-12">
-              <label htmlFor="passConfirm" className="form-label">
-                Confirm password
-              </label>
-              <input
-                type="password"
-                className="form-control"
-                id="passConfirm"
-                name="passConfirm"
-                ref={confirmElement}
-                required
-              />
-              <div className="invalid-feedback" id="ConfirmPasswordFeedback">
-                {data
-                  ? data.errors.map((item, index) => {
-                      if (item.param === "confirmPass") {
-                        confirmElement.current.value = "";
-                        return `${item.msg}. `;
-                      } else {
-                        return "";
-                      }
-                    })
-                  : "confirm password must match password"}
+              <div className="col-12">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  ref={emailElement}
+                  required
+                />
+                <div className="invalid-feedback" id="EmailFeedback">
+                  {data
+                    ? data.errors.map((item, index) => {
+                        if (item.param === "email") {
+                          emailElement.current.value = "";
+                          return `${item.msg}. `;
+                        } else {
+                          return "";
+                        }
+                      })
+                    : "email must be valid"}
+                </div>
+                <div className="valid-feedback"></div>
               </div>
-              <div className="valid-feedback"></div>
-            </div>
-            <div className="col-12">
-              <label htmlFor="email" className="form-label">
-                Email
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="email"
-                name="email"
-                ref={emailElement}
-                required
-              />
-              <div className="invalid-feedback" id="EmailFeedback">
-                {data
-                  ? data.errors.map((item, index) => {
-                      if (item.param === "email") {
-                        emailElement.current.value = "";
-                        return `${item.msg}. `;
-                      } else {
-                        return "";
-                      }
-                    })
-                  : "email must be valid"}
-              </div>
-              <div className="valid-feedback"></div>
-            </div>
 
-            <div className="col-12">
-              <button
-                className="btn btn-primary mb-2"
-                onClick={handleSubmit}
-                type="submit"
-              >
-                Submit form
-              </button>
-            </div>
-          </form>
+              <div className="col-12">
+                <button
+                  className="btn btn-primary mb-2"
+                  onClick={handleSubmit}
+                  type="submit"
+                >
+                  Submit form
+                </button>
+              </div>
+            </form>
+          </section>
         </section>
       </section>
     </>
